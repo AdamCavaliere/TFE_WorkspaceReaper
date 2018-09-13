7707fd6e254c67003bfbae702e42dcb2fb12f435
Adam Cavaliere <adam.cavaliere@gmail.com>
2018-09-12 22:47:56 -0500

    updated for sqs

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,18 +22,65 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
-resource "aws_lambda_function" "test_lambda" {
-  filename         = "../functions/reaper.zip"
-  function_name    = "FindWorkspacesToReap"
-  role             = "${aws_iam_role.iam_for_lambda.arn}"
-  handler          = "reapWorkspaces.findReapableWorkspaces"
-  runtime          = "python3.6"
+resource "aws_iam_role" "sqs_for_lambda" {
+  name = "sqs_for_lambda-${var.TFE_ORG}"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "sqs:ListQueues",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "sqs:*",
+            "Resource": "arn:aws:sqs:*:*:*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "reaper_lambda" {
+  filename      = "../functions/reaper.zip"
+  function_name = "FindWorkspacesToReap"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "reapWorkspaces.findReapableWorkspaces"
+  runtime       = "python3.6"
 
   environment {
     variables = {
       TFE_TOKEN = "${var.TFE_TOKEN}"
       TFE_URL   = "${var.TFE_URL}"
       TFE_ORG   = "${var.TFE_ORG}"
+      SQS_QUEUE = "${aws_sqs_queue.reaper_queue.id}"
     }
   }
+}
+
+resource "aws_lambda_function" "reaper_lambda" {
+  filename      = "../functions/reaper.zip"
+  function_name = "ProcessReaperQueue"
+  role          = "${aws_iam_role.sqs_for_lambda.arn}"
+  handler       = "reapWorkspaces.processQueue"
+  runtime       = "python3.6"
+
+  environment {
+    variables = {
+      TFE_TOKEN = "${var.TFE_TOKEN}"
+      TFE_URL   = "${var.TFE_URL}"
+      TFE_ORG   = "${var.TFE_ORG}"
+      SQS_QUEUE = "${aws_sqs_queue.reaper_queue.id}"
+    }
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "event_source_mapping" {
+  batch_size       = 10
+  event_source_arn = "${aws_sqs_queue.reaper_queue.arn}"
+  enabled          = true
+  function_name    = "${aws_lambda_function.reaper_lambda.arn}"
 }
