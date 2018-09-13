a69ef027e561ea0de0cbf68384cc26a7aa3d5ca2
Adam Cavaliere <adam.cavaliere@gmail.com>
2018-09-13 07:55:51 -0500

    fixed mapping

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,5 +100,5 @@ resource "aws_lambda_event_source_mapping" "event_source_mapping" {
   batch_size       = 10
   event_source_arn = "${aws_sqs_queue.reaper_queue.arn}"
   enabled          = true
-  function_name    = "${aws_lambda_function.reaper_lambda.arn}"
+  function_name    = "${aws_lambda_function.process_lambda.arn}"
 }
