1d9f88b3cc0ae61447f12aa04cd8d6b7521736d2
Adam Cavaliere <adam.cavaliere@gmail.com>
2018-09-26 15:52:22 -0500

    removed depends on

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -27,11 +27,6 @@ resource "aws_api_gateway_integration" "lambda" {
 }
 
 resource "aws_api_gateway_deployment" "example" {
-  depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
-  ]
-
   rest_api_id = "${aws_api_gateway_rest_api.reaperui.id}"
   stage_name  = "publish"
 }
