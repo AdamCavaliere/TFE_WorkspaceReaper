248831a0d33a9753033912d0a43a3afe90649408
Adam Cavaliere <adam.cavaliere@gmail.com>
2018-09-26 15:42:11 -0500

    updated policy

--- terraform/S3.tf
+++ terraform/S3.tf
@@ -2,6 +2,15 @@ resource "aws_s3_bucket" "visual_results" {
   bucket = "workspacereaper-${var.TFE_ORG}.this-demo.rocks"
   acl    = "public-read"
 
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "getitall" {
+  bucket = "${aws_s3_bucket.visual_results.id}"
+
   policy = <<EOF
 {
   "Version":"2012-10-17",
@@ -16,9 +25,4 @@ resource "aws_s3_bucket" "visual_results" {
   ]
 }
 EOF
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
 }
