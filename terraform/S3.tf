b0de735201b76c48b934c22dcc37d1e8715b76f0
Adam Cavaliere <adam.cavaliere@gmail.com>
2018-09-26 15:33:21 -0500

    updated website settings

--- terraform/S3.tf
+++ terraform/S3.tf
@@ -20,16 +20,5 @@ EOF
   website {
     index_document = "index.html"
     error_document = "error.html"
-
-    routing_rules = <<EOF
-[{
-    "Condition": {
-        "KeyPrefixEquals": "docs/"
-    },
-    "Redirect": {
-        "ReplaceKeyPrefixWith": "documents/"
-    }
-}]
-EOF
   }
 }
