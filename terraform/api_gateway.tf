666ac6734962de4bcb5a30b8d630c27aea44208d
Adam Cavaliere <adam.cavaliere@gmail.com>
2018-09-26 16:08:04 -0500

    updated api_Gateway

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -30,3 +30,14 @@ resource "aws_api_gateway_deployment" "example" {
   rest_api_id = "${aws_api_gateway_rest_api.reaperui.id}"
   stage_name  = "publish"
 }
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.reaper_ui.arn}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_deployment.example.execution_arn}/*/*"
+}
