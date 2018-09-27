84695ed30b29064e3746b7e13ee06c226367754c
Adam Cavaliere <adam.cavaliere@gmail.com>
2018-09-26 21:52:44 -0500

    created

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_rest_api" "reaperui" {
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = "${aws_api_gateway_rest_api.reaperui.id}"
   parent_id   = "${aws_api_gateway_rest_api.reaperui.root_resource_id}"
-  path_part   = "{proxy+}"
+  path_part   = "awesome"
 }
 
 resource "aws_api_gateway_method" "proxy" {
@@ -28,7 +28,7 @@ resource "aws_api_gateway_integration" "lambda" {
 
 resource "aws_api_gateway_deployment" "example" {
   rest_api_id = "${aws_api_gateway_rest_api.reaperui.id}"
-  stage_name  = "publish"
+  stage_name  = "Production"
 }
 
 resource "aws_lambda_permission" "apigw" {
