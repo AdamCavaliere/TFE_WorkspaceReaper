59cbdacee1f332dacf3f6b7616e5f6d35f4e9625
Adam Cavaliere <adam.cavaliere@gmail.com>
2018-09-26 22:04:03 -0500

    root path

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -6,7 +6,6 @@ resource "aws_api_gateway_rest_api" "reaperui" {
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = "${aws_api_gateway_rest_api.reaperui.id}"
   parent_id   = "${aws_api_gateway_rest_api.reaperui.root_resource_id}"
-  path_part   = "awesome"
 }
 
 resource "aws_api_gateway_method" "proxy" {
