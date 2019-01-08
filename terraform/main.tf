73e6dabc4f3d8a3190fd7cf72f54fbabf74de683
Adam Cavaliere <adam.cavaliere@gmail.com>
2019-01-08 12:06:31 -0600

    fixed hash

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ resource "aws_lambda_function" "reaper_lambda" {
   filename         = "../functions/reaper.zip"
   function_name    = "FindWorkspacesToReap-${var.TFE_ORG}"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
-  source_code_hash = "${base64sha256(file("../functions/reaper.zip"))}"
+  source_code_hash = "${data.archive_file.reaper.output_base64sha256}"
   handler          = "reapWorkspaces.findReapableWorkspaces"
 
   runtime = "python3.6"
@@ -34,7 +34,7 @@ resource "aws_lambda_function" "process_lambda" {
   filename         = "../functions/reaper.zip"
   function_name    = "ProcessReaperQueue-${var.TFE_ORG}"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
-  source_code_hash = "${base64sha256(file("../functions/reaper.zip"))}"
+  source_code_hash = "${data.archive_file.reaper.output_base64sha256}"
   handler          = "reapWorkspaces.processQueue"
 
   runtime = "python3.6"
